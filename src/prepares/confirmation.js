3edab428d0709eecd48839fae9a465b8f968594f
tomoya.shimomura <tomoya.shimomura@wireframes.co.jp>
2021-08-24 17:13:51 +0900

    refactor prepare function confirmation

--- src/prepares/confirmation.js
+++ src/prepares/confirmation.js
@@ -6,11 +6,9 @@
  * @returns {Rule}
  */
 export default function (values, key, rule) {
-  let confirmation_key = key + '_confirmation';
-  if (values[confirmation_key] === undefined) {
-    rule[1] = '';
-  } else {
-    rule[1] = values[confirmation_key];
-  }
+  const confirmation_key = `${key}_confirmation`;
+
+  rule[1] = values[confirmation_key] === undefined ? '' : values[confirmation_key];
+
   return rule;
 }
